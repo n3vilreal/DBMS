4d310d19fb9d18f6ecc8e7cd339c63e857e0ffed
n3vilreal <nebil.241423@ncit.edu.np>
2026-06-05 11:19:21 +0545

    Lab 3 incomplete

---
+++ college-lab/lab3.sql
@@ -0,0 +1,77 @@
+-- Check and create database
+show DATABASEs;
+CREATE DATABASE lab3;
+use lab3;
+show TABLEs;
+
+-- Part 1: database setup
+
+-- schema creation
+CREATE TABLE Students (
+    student_id INT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    age INT,
+    email VARCHAR(100),
+    phone_number VARCHAR(20)
+);
+
+-- Create Courses Table
+CREATE TABLE courses (
+    course_id INT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    credit_hour INT
+);
+
+-- Create Enrollment Table
+CREATE TABLE enrollment (
+    enrollment_id INT PRIMARY KEY,
+    course_id INT,
+    student_id INT,
+    FOREIGN KEY (course_id) REFERENCES courses(course_id),
+    FOREIGN KEY (student_id) REFERENCES Students(student_id)
+);
+
+
+-- dummy data insertion 
+INSERT INTO Students (student_id, name, age, email, phone_number) VALUES
+(1, 'Alice Smith', 20, 'alice@example.com', '555-0101'),
+(2, 'Bob Johnson', 22, 'bob@example.com', '555-0102'),
+(3, 'Charlie Brown', 19, 'charlie@example.com', '555-0103'),
+(4, 'Diana Prince', 21, 'diana@example.com', '555-0104'),
+(5, 'Evan Wright', 23, 'evan@example.com', '555-0105');
+
+-- Insert Data into Courses
+INSERT INTO courses (course_id, name, credit_hour) VALUES
+(101, 'Introduction to Computer Science', 3),
+(102, 'Data Structures', 4),
+(103, 'Database Management Systems', 3),
+(104, 'Web Development', 3),
+(105, 'Artificial Intelligence', 4);
+
+-- Insert Data into Enrollment
+INSERT INTO enrollment (enrollment_id, course_id, student_id) VALUES
+(1001, 101, 1),
+(1002, 103, 1),
+(1003, 102, 2),
+(1004, 104, 2),
+(1005, 101, 3),
+(1006, 105, 4),
+(1007, 103, 5),
+(1008, 105, 5),
+(1009, 102, 1);
+
+-- part 2: sql practice questions
+
+-- basic selection and filtering
+SELECT * from students;
+select name, email from students;
+select * from students where age>20;
+SELECT * from students where name like 'a%';
+SELECT * from courses where name like '%science%';
+select * from students order by age desc;
+select * from students where phone_number='555-0103';
+
+-- CURD OPERATIONS
+ALTER TABLE students ADD address VARCHAR(255);
+ALTER TABLE students ADD is_active BOOLEAN DEFAULT TRUE;
+ALTER TABLE students MODIFY COLUMN phone_number VARCHAR(50);
